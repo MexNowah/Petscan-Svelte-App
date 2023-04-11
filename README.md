2f94b591c755d15eca64b0a71452e6f5d6a647a7
MexNowah <99980127+MexNowah@users.noreply.github.com>
2023-04-11 07:06:25 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Petscan-Svelte-App
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/sveltejs-kit-template-default-ashvhr)
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+
+	import './styles.css';
+  import "../app.css";
+
+</script>
+
+<div class="app">
+
+	<main>
+		<slot />
+	</main>
+
+</div>
+
+<style>
+	
+</style>
